3f160bb93f6b70e45179b58391dcaa6f57657ad1
LilianXeyman <elisa0101232478@gmail.com>
2025-02-11 19:27:35 +0000

    UI
    Hecha gran parte de la UI
    Falta el post-procesado y menu de opciones

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,15 +3,55 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    //Para cambiar la escena
+    public string SampleScene;
+
     [SerializeField]
     GameObject gameTitle;
     [SerializeField]
     GameObject enterStart;
     [SerializeField]
     GameObject imageCargando;
+    [SerializeField]
+    GameObject botonEmpezar;
+    [SerializeField]
+    GameObject botonOpciones;
+    [SerializeField]
+    GameObject botonSalir;
+    [SerializeField]
+    GameObject imagenPlayer;
+    [SerializeField]
+    GameObject imagenEnemy;
+    [SerializeField]
+    GameObject imagenVs1;
+    [SerializeField]
+    GameObject imagenVs2;
+
+    //Parámetros
+    [SerializeField]
+    float posPantallaBotonEmpezar;
+    [SerializeField]
+    float posPantallaBotonOpciones;
+    [SerializeField]
+    float posPantallaBotonSalir;
+    [SerializeField]
+    float tiempoAnim;
+    [SerializeField]
+    LeanTweenType curvAnim;
+    [SerializeField]
+    float posPantallaImagenPlayer;
+    [SerializeField]
+    float posPantallaImagenEnemy;
+    [SerializeField]
+    float posPantallaImagenVs1;
+    [SerializeField]
+    float posPantallaImagenVs2;
+    [SerializeField]
+    LeanTweenType curvAnim2;
 
     //Imput System
     PlayerInput _playerInput;
@@ -31,6 +71,17 @@ public class Menu : MonoBehaviour
 
         //Para el enter
         menuActivo = true;
+
+        //Botones
+        LeanTween.moveLocalY(botonEmpezar, -1200, 0);
+        LeanTween.moveLocalY(botonOpciones, -1200, 0);
+        LeanTween.moveLocalY(botonSalir, -1200, 0);
+
+        //Imagenes Menu
+        LeanTween.moveLocalY(imagenPlayer, 1200, 0);
+        LeanTween.moveLocalY(imagenEnemy, -1200, 0);
+        LeanTween.moveLocalY(imagenVs1, -1200, 0);
+        LeanTween.moveLocalY(imagenVs2, 1200, 0);
     }
         // Update is called once per frame
         void Update()
@@ -43,8 +94,8 @@ public class Menu : MonoBehaviour
             //Para la primera parte del menu
             if (Input.GetButtonDown("Submit") || Input.anyKeyDown)
             {
-                LeanTween.cancel(enterStart);
-                enterStart.SetActive(false);
+                
+                ShowButtons();
             //LeanTween.
                 Debug.Log("Empezar");
             }
@@ -56,6 +107,41 @@ public class Menu : MonoBehaviour
                 Screen.fullScreen = !Screen.fullScreen;
             }
         }
+    void ShowButtons()
+    {
+        //Para el parpadeo del título
+        LeanTween.cancel(enterStart);
+        enterStart.SetActive(false);
+        gameTitle.SetActive(false);
+
+        //Botones
+        LeanTween.moveLocalY(botonEmpezar, posPantallaBotonEmpezar, tiempoAnim).setEase(curvAnim).setOnComplete(() => { 
+            LeanTween.moveLocalY(botonOpciones, posPantallaBotonOpciones, tiempoAnim).setEase(curvAnim).setOnComplete(() => {
+                LeanTween.moveLocalY(botonSalir, posPantallaBotonSalir, tiempoAnim ).setEase(curvAnim).setOnComplete(() => {
+                    //Imagenes robots
+                    LeanTween.moveLocalY(imagenPlayer, posPantallaImagenPlayer, tiempoAnim).setEase(curvAnim2).setOnComplete(() => {
+                        LeanTween.moveLocalY(imagenVs1, posPantallaImagenVs1, tiempoAnim * 0.25f).setEase(curvAnim2).setOnComplete(() => {
+                            LeanTween.moveLocalY(imagenVs2, posPantallaImagenVs2, tiempoAnim * 0.25f).setEase(curvAnim2).setOnComplete(() => {
+                                LeanTween.moveLocalY(imagenEnemy, posPantallaImagenEnemy, tiempoAnim).setEase(curvAnim2);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+    public void BotonSalir()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Detiene el juego en el editor
+#else
+        Application.Quit(); // Cierra la aplicación en la build
+#endif
+    }
+    public void Comenzar()
+    {
+        SceneManager.LoadScene(SampleScene);
+    }
     /*void AlternarMenu()
     { 
         menuActivo=!menuActivo;
